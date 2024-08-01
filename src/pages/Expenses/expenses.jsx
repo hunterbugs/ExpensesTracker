533de3eae97ff5ebc9d7ec974c85e2043066f49a
hunterbugs <hunter_bugs@yahoo.com>
2024-08-01 15:39:59 +0800

    Update expenses.jsx

---
+++ src/pages/Expenses/expenses.jsx
@@ -0,0 +1,250 @@
+import './Home1.css'
+
+export default function Home1() {
+  return (
+    <div className="home">
+      <div className="container-8">
+        <div className="container-9">
+          <div className="logo">
+            <div className="icon">
+              <img className="group" src="assets/vectors/Group36_x2.svg" />
+            </div>
+            <span className="expense-tracker">
+              ExpenseTracker
+            </span>
+          </div>
+          <div className="menu">
+            <div className="all-expense">
+              <span className="all-expense-1">
+                All Expense
+              </span>
+            </div>
+            <div className="all-income">
+              <span className="all-income-1">
+                All  Income
+              </span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="image">
+            </div>
+            <div className="alex-rybachok">
+              Alex Rybachok
+            </div>
+            <div className="chevron-down">
+              <img className="vector-3" src="assets/vectors/Vector8_x2.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="container-11">
+          <div className="container-10">
+            <div className="expense-log">
+              Expense Log
+            </div>
+            <div className="capture-and-organize-every-penny-spent-with-ease-aclear-view-of-your-financial-habits-at-your-fingertips">
+              Capture and organize every penny spent with ease! A clear view of your financial habits at your fingertips.
+            </div>
+            <div className="block-6">
+              <div className="total-income">
+                <div className="container-6">
+                  <img className="arrow-15" src="assets/vectors/Arrow1534_x2.svg" />
+                </div>
+                <div className="text">
+                  <div className="total-income-1">
+                    Total Income
+                  </div>
+                  <span className="container-6">
+                    $632.000
+                  </span>
+                </div>
+              </div>
+              <div className="total-expense">
+                <div className="container-3">
+                  <img className="arrow-14" src="assets/vectors/Arrow1413_x2.svg" />
+                </div>
+                <div className="text-1">
+                  <div className="total-expense-1">
+                    Total Expense
+                  </div>
+                  <span className="container-7">
+                    $632.000
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="block">
+              <div className="rectangle-1">
+              </div>
+              <img className="ellipse-1" src="assets/vectors/Ellipse110_x2.svg" />
+              <img className="ellipse-30" src="assets/vectors/Ellipse3014_x2.svg" />
+              <img className="ellipse-28" src="assets/vectors/Ellipse2816_x2.svg" />
+              <img className="ellipse-29" src="assets/vectors/Ellipse2911_x2.svg" />
+              <div className="expenses-categories">
+                Expenses categories
+              </div>
+              <div className="container-2">
+                <div className="list">
+                  <div className="hobby">
+                    <div className="block-1">
+                      <div className="ellipse-25">
+                      </div>
+                      <span className="hobby-1">
+                        Hobby
+                      </span>
+                    </div>
+                    <div className="container-1">
+                      45%
+                    </div>
+                  </div>
+                  <div className="products">
+                    <div className="block-2">
+                      <div className="ellipse-251">
+                      </div>
+                      <span className="products-1">
+                        Products
+                      </span>
+                    </div>
+                    <div className="container-2">
+                      25%
+                    </div>
+                  </div>
+                  <div className="cinema">
+                    <div className="block-3">
+                      <div className="ellipse-252">
+                      </div>
+                      <span className="cinema-1">
+                        Cinema
+                      </span>
+                    </div>
+                    <div className="container-3">
+                      20%
+                    </div>
+                  </div>
+                  <div className="health">
+                    <div className="block-4">
+                      <div className="ellipse-253">
+                      </div>
+                      <span className="health-1">
+                        Health
+                      </span>
+                    </div>
+                    <div className="container-4">
+                      10%
+                    </div>
+                  </div>
+                </div>
+                <div className="container-5">
+                  <div className="rectangle-5816">
+                  </div>
+                </div>
+              </div>
+              <span className="container">
+                100%
+              </span>
+            </div>
+          </div>
+          <div className="container-4">
+            <div className="block-7">
+              <div className="income-1">
+                <div className="radio-button">
+                  <div className="container-12">
+                    <div className="circle">
+                    </div>
+                  </div>
+                </div>
+                <div className="expense">
+                  Expense
+                </div>
+              </div>
+              <div className="expense-1">
+                <div className="radio-button-1">
+                  <div className="container-13">
+                  </div>
+                </div>
+                <div className="income">
+                  Income
+                </div>
+              </div>
+            </div>
+            <div className="block-5">
+              <div className="date">
+                <div className="date-1">
+                  Date
+                </div>
+                <div className="input">
+                  <span className="mmddyyyy">
+                    mm/dd/yyyy
+                  </span>
+                  <img className="calendar" src="assets/vectors/Calendar17_x2.svg" />
+                </div>
+              </div>
+              <div className="time">
+                <div className="time-1">
+                  Time
+                </div>
+                <div className="input-1">
+                  <span className="container-5">
+                    00:00:00
+                  </span>
+                  <img className="clock" src="assets/vectors/Clock15_x2.svg" />
+                </div>
+              </div>
+            </div>
+            <div className="category">
+              <div className="category-1">
+                Category
+              </div>
+              <div className="input-2">
+                <span className="different">
+                  Different
+                </span>
+              </div>
+            </div>
+            <div className="sum">
+              <div className="sum-1">
+                Sum
+              </div>
+              <div className="input-3">
+                <span className="enter-the-sum">
+                  Enter the sum
+                </span>
+                <span className="uah">
+                  UAH
+                </span>
+              </div>
+            </div>
+            <div className="comment">
+              <div className="comment-1">
+                Comment
+              </div>
+              <div className="input-4">
+                <span className="enter-the-text">
+                  Enter the text
+                </span>
+              </div>
+            </div>
+            <div className="btn">
+              <span className="add">
+                Add
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-7">
+        <div className="container">
+          <img className="user-1" src="assets/vectors/User8_x2.svg" />
+          <span className="profile-settings">
+            Profile settings
+          </span>
+        </div>
+        <div className="container-1">
+          <img className="log-out-1" src="assets/vectors/LogOut5_x2.svg" />
+          <span className="log-out">
+            Log out
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
